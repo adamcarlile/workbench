7753cb851937d39d1cd71c053529a7113b99ad95
Adam Carlile <adam@benchmedia.co.uk>
2011-04-17 21:44:53 +0100

    Added update to enquiries

--- app/controllers/public/enquiries_controller.rb
+++ app/controllers/public/enquiries_controller.rb
@@ -7,8 +7,10 @@ class Public::EnquiriesController < Public::BaseController
     if request.post?
       @enquiry.attributes = params[:enquiry]
       if @enquiry.valid? && (ENV['RAILS_ENV'] == 'cucumber' || verify_recaptcha(@enquiry))
+        SiteMailer.deliver_enquiry(@enquiry)
         @enquiry.save
-        redirect_to url_for_page(@page, :sent => 1) and return
+        flash[:notice] = 'Thanks for getting in touch, we\'ll get back to you'
+        redirect_to url_for_page(@page) and return
       else
         flash.now[:error] = 'Please check the details you provided'
         render_page
