8a28d388ce75cdaa44eb443018ad906eb44bcc5f
Adam Carlile <adam@benchmedia.co.uk>
2011-04-24 22:14:54 +0100

    Added updated environment.rb to include working gems

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,11 @@ config.plugins = [ :find_by_param, :all ]
 
 config.autoload_paths += %W( #{RAILS_ROOT}/vendor/plugins/cms-engine/app/models/page_types )
 
-config.gem "mislav-will_paginate", :version => ">= 2.3.0", :lib => "will_paginate", :source => "http://gems.github.com"
+config.gem "will_paginate"
 config.gem "authlogic", :version => "<= 2.1.3"
 config.gem "resource_controller", :version => ">= 0.6.6"
 config.gem 'RedCloth', :lib => 'redcloth'
-config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+config.gem "factory_girl"
 config.gem 'faker'
 config.gem 'cucumber'
 config.gem 'ancestry'
